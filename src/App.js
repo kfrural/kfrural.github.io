19ffa1d9415acd48185fbdb67111433b49cd1d91
kfrural <karlinharural@gmail.com>
2024-09-17 09:14:26 -0300

    alterado

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Project from './components/project';
 import About from './components/About';
 import Experience from './components/experiencia';
 import Formation from './components/formation';
-import Hobbies from './components/hobbies';
+//import Hobbies from './components/hobbies';
 import Courses from './components/courses';
 import Event from './components/eventos';
 import Hero from './components/hero';
 import Tecnologias from './components/tecnologias';
-import Skills from './components/skills';
+//import Skills from './components/skills';
 import ContactComponent from './components/contact';
 import Functions from './components/functions';
 
